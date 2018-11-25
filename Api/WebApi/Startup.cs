e502e8173bb5975bc5f14fc097c09297e23823ae
Kareem <kareemmuhammad924@gmail.com>
2018-11-25 17:58:09 +0200

    Add swagger
    Add swagger

--- Api/WebApi/Startup.cs
+++ Api/WebApi/Startup.cs
@@ -11,6 +11,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Data;
+using Swashbuckle.AspNetCore.Swagger;
+
 namespace WebApi
 {
     public class Startup
@@ -28,6 +30,27 @@ namespace WebApi
             Data.Configure.ConfigureServices(services,Configuration.GetConnectionString("defaultConnection"));
             Application.Configure.ConfigureServices(services);
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "ToDo API",
+                    Description = "A simple example ASP.NET Core Web API",
+                    TermsOfService = "None",
+                    Contact = new Contact
+                    {
+                        Name = "Shayne Boyer",
+                        Email = string.Empty,
+                        Url = "https://twitter.com/kareem924"
+                    },
+                    License = new License
+                    {
+                        Name = "Use under LICX",
+                        Url = "https://example.com/license"
+                    }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,7 +64,13 @@ namespace WebApi
             {
                 app.UseHsts();
             }
-
+            
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My Project API");
+                //c.RoutePrefix = string.Empty;
+            });
             app.UseHttpsRedirection();
             app.UseMvc();
         }
