a90ec0e8d05ce5feabbd86f9d6cbdb301704f0c4
Kareem <kareemmuhammad924@gmail.com>
2018-11-29 09:48:26 +0200

    Add AutoMapper

---
+++ Api/Application/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using Abstract.Entities;
+using Application.Dto;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application
+{
+   public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Users, UserDto>();
+            CreateMap<UserDto, Users>();
+        }
+    }
+}
